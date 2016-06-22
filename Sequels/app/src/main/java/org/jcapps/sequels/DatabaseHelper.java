62a8b3e3a997a0dfa58cbde232c0bdd156fc4926
Jeff Chin <dizaburrow@gmail.com>
2016-06-22 11:32:16 -0500

    Sequels
    Add Seed/insert

--- Sequels/app/src/main/java/org/jcapps/sequels/DatabaseHelper.java
+++ Sequels/app/src/main/java/org/jcapps/sequels/DatabaseHelper.java
@@ -11,6 +11,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final int DB_VERSION = 1; // change as we upgrade
     public static final String DB_NAME = "ReallyBadSequels.db"; // name of your db
+    public static final String CREATE_TABLE = "CREATE TABLE sequels ( id INT PRIMARY KEY, name TEXT);";
+    public static final String DROP_TABLE = "DROP TABLE IF EXISTS sequels;";
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -18,12 +20,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE); // create table
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DROP_TABLE); // drop table
+        onCreate(db); // create table
+    }
+
+    // NOT an override
+    public void seedDatabase() {
 
     }
+
+    public void insertSequek(int id, String name) {
+        
+    }
+
 }
 
